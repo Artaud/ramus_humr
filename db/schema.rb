03dacac2cfad88fa021aadbe980ae80d02b0b158
Prelang Builder <builder@prelang.com>
2014-08-30 20:56:39 +0200

    Executed 'rake db:migrate' (development and test) to migrate all generated migrations which created schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140830185140) do
+ActiveRecord::Schema.define(version: 20140830185155) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,5 +45,33 @@ ActiveRecord::Schema.define(version: 20140830185140) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "akces", force: true do |t|
+    t.datetime "termin"
+    t.text     "popis"
+    t.text     "cena"
+    t.text     "misto_srazu"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "prihlaskas", force: true do |t|
+    t.string   "jmeno_prijmeni"
+    t.string   "kontakt"
+    t.integer  "role_ucastnika_id"
+    t.string   "vzkaz"
+    t.integer  "akce_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "prihlaskas", ["akce_id"], name: "index_prihlaskas_on_akce_id"
+  add_index "prihlaskas", ["role_ucastnika_id"], name: "index_prihlaskas_on_role_ucastnika_id"
+
+  create_table "role_ucastnikas", force: true do |t|
+    t.string   "nazev"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
